2c58eb2e77a33207c21f337aea4c1074c713ee74
Nhật Quang <nhatquanglgbg@gmail.com>
2025-09-10 21:27:31 +0700

    Add Sign Out

--- app/src/main/java/com/example/carez/view/MainActivity.kt
+++ app/src/main/java/com/example/carez/view/MainActivity.kt
@@ -8,9 +8,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.carez.R
+import com.example.carez.auth.GoogleAuthClient
+import com.example.carez.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var biding: ActivityMainBinding
+    private var googleAuthClient = GoogleAuthClient(this@MainActivity)
 
     companion object {
         fun onStart(context: Context) {
@@ -22,11 +28,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        biding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(biding.root)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        biding.btnSignOut.setOnClickListener {
+            lifecycleScope.launch {
+            googleAuthClient.signOut()
+                    SignInActivity.onStart(this@MainActivity)
+                    finish()
+            }
         }
     }
+
 }
