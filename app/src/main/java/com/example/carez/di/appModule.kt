50742358dc20a8e032f9c79280fef88f7a9275af
Nhật Quang <nhatquanglgbg@gmail.com>
2025-09-21 21:21:23 +0700

    add splash screen activity and view model, update manifest and dependency injection to include sign-in check logic

--- app/src/main/java/com/example/carez/di/appModule.kt
+++ app/src/main/java/com/example/carez/di/appModule.kt
@@ -4,10 +4,13 @@ import com.example.carez.data.remote.datasource.GoogleAuthDataSource
 import com.example.carez.data.remote.datasource.GoogleAuthDataSourceImpl
 import com.example.carez.data.repository.UserRepositoryImpl
 import com.example.carez.domain.repository.UserRepository
+import com.example.carez.domain.usecase.CheckSignInUseCase
 import com.example.carez.domain.usecase.SignInWithGoogleUseCase
 import com.example.carez.domain.usecase.SignOutUseCase
 import com.example.carez.presentation.activity.main.MainViewModel
 import com.example.carez.presentation.activity.signIn.SignInViewModel
+import com.example.carez.presentation.activity.splash.SplashActivity
+import com.example.carez.presentation.activity.splash.SplashViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -22,8 +25,10 @@ val appModule = module {
     // UseCase
     factory { SignInWithGoogleUseCase(get()) }
     factory { SignOutUseCase(get()) }
+    factory { CheckSignInUseCase(get()) }
 
     // ViewModel
     viewModel { SignInViewModel(get()) }
     viewModel { MainViewModel(get()) }
+    viewModel { SplashViewModel(get()) }
 }
